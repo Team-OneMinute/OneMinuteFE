f718947d68b2d2a6aba6545c15e59cc393a03bb0
shuta <sdsds.com@hotmail.co.jp>
2024-03-20 00:25:08 +0900

    add email&password auth.

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,34 +1,59 @@
 'use client';
 import firebase from 'firebase/compat/app';
+import { Auth, signOut,  User as AuthUser, getAuth as getAuthFromFirebase, onAuthStateChanged } from 'firebase/auth';
 import * as firebaseui from 'firebaseui';
 import 'firebaseui/dist/firebaseui.css';
-import { onAuthStateChanged } from 'firebase/auth';
-import { getAuth } from '@/app/service/auth';
 import { useEffect } from 'react';
+import { firebaseConfig } from '../../infrastructure/firebase/firebaseConfig';
 
 // TODO: メールリンク認証によって飛んでくるメールのテンプレート変更
 export default function LoginPage() {
-    useEffect(() => {
-        const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
+    // TODO: password mail
+    // https://qiita.com/mml/items/5e325bb19ba532ca56b7
+    const logout = async () => {
+        const app = firebase.initializeApp(firebaseConfig);
+        const auth = getAuthFromFirebase(app);
+        await signOut(auth);
+    };
 
+    useEffect(() => {
+        const app = firebase.initializeApp(firebaseConfig);
+        const auth = getAuthFromFirebase(app);
+
+        firebase.auth().onAuthStateChanged((user) => {
+            console.log('authentication');
+            if (user != null) {
+                if (user.emailVerified == false) {
+                    console.log('send verify mail');
+                    console.log(user);
+                    user.sendEmailVerification();
+                } else {
+                    console.log('success login');
+                    console.log(user);
+                }
+            } else {
+                console.log('login error');
+            }
+        });
+
+        const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
         ui.start('#firebaseui-auth-container', {
             callbacks: {
                 signInSuccessWithAuthResult: function (authResult, redirectUrl) {
                     // Action if the user is authenticated successfully
-                    return true;
+                    return false;
                 },
                 uiShown: function () {
                     // This is what should happen when the form is full loaded. In this example, I hide the loader element.
                     document.getElementById('loader')!.style.display = 'none';
                 },
             },
-            signInSuccessUrl: 'https://localhost/', // This is where should redirect if the sign in is successful.
+            signInSuccessUrl: 'login', // This is where should redirect if the sign in is successful.
             signInOptions: [
                 // This array contains all the ways an user can authenticate in your application. For this example, is only by email.
                 {
                     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                    signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-                    requireDisplayName: true,
+                    //signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
                 },
             ],
             tosUrl: 'https://www.example.com/terms-conditions', // URL to you terms and conditions.
@@ -42,7 +67,8 @@ export default function LoginPage() {
     return (
         <div>
             <div id='firebaseui-auth-container'>login Page</div>
-            <div id="loader">Now Loading...</div>
+            <div id='loader'>Now Loading...</div>
+            <div onClick={() => logout()}> log out </div>
         </div>
     );
 }
