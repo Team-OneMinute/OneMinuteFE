c50d142d6b62a2e4b530102557a827fc0a8b3df6
shuta <sdsds.com@hotmail.co.jp>
2024-04-10 17:27:57 +0900

    add tokenID set api

---
+++ src/app/api/transaction/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from 'next/server';
+import ethers, { Contract, Interface, JsonRpcProvider } from 'ethers';
+//import abi from "./abi";
+import abi from './abi.json' assert { type: 'json' };
+
+export const GET = async (req: NextRequest) => {
+    try {
+        const walletAddress = '0x18aEFe337E68f5Bd3e18Ba09960f9136754590a6';
+        const characterNftContractAddress = '0x093d8549D8cBcF5844B23f508ac2c1687E92862D';
+
+        const zKyotoProvider = new JsonRpcProvider('https://rpc.startale.com/zkyoto');
+
+        const smartWalletContract = new Contract(characterNftContractAddress, abi, zKyotoProvider);
+
+        const totalNftSupply = await smartWalletContract.totalSupply();
+        const totalNftSupplyInt = Number(totalNftSupply);
+        console.log(totalNftSupply);
+
+        let expectAddress;
+        let tokenId;
+        for (let i = 0; i < totalNftSupplyInt; i++) {
+            let response = await smartWalletContract.ownerOf(i);
+            if (walletAddress == response) {
+                tokenId = i;
+                expectAddress = response;
+                break;
+            }
+        }
+        console.log(tokenId);
+        console.log(expectAddress);
+
+        return NextResponse.json(totalNftSupplyInt);
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json('111');
+    }
+};
